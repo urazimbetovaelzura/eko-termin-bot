8a6bceaadca06df1ea722e7db34fc8c1ab5d6221
urazimbetovaelzura <urazimbetovaelzura@gmail.com>
2026-05-26 02:14:49 +0500

    Update bot_eko.py

--- bot_eko.py
+++ bot_eko.py
@@ -331,7 +331,6 @@ async def start_web_server():
     await runner.setup()
 
     port = int(os.environ.get("PORT", 10000))
-
     site = web.TCPSite(runner, "0.0.0.0", port)
     await site.start()
     
@@ -341,15 +340,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-async def handle(request):
-    return web.Response(text="Bot is running!")
-
-async def start_web_server():
-    app = web.Application()
-    app.router.add_get('/', handle)
-
-    runner = web.AppRunner(app)
-    await runner.setup()
-
-    site = web.TCPSite(runner, '0.0.0.0', 10000)
-    await site.start()
